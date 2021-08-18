e19c3ecac86dec9647e0235a113a2b8f152a215b
suwur <85727752+suwur@users.noreply.github.com>
2021-08-18 12:27:15 +0700

    Update rand.js

--- rand.js
+++ rand.js
@@ -4,28 +4,27 @@ rrt6[1] = " mendapatkan ";
 rrt6[2] = " memperoleh ";
 rrt6[3] = " dapat ";
 rrt6[4] = " diberikan ";
-var i = Math.floor(5*Math.random());
+
 var rtt7 = new Array ();
 rtt7[0] = " info ";
 rtt7[1] = " informasi ";
 rtt7[2] = " balasan ";
 rtt7[3] = " kabar ";
 rtt7[4] = " data ";
-var i = Math.floor(5*Math.random());
+
 var rrt8 = new Array ();
 rrt8[0] = "Tersedia ";
 rrt8[1] = "Siap Jual ";
 rrt8[2] = "Menyediakan ";
 rrt8[3] = "Siap Menjual ";
 rrt8[4] = "Siap suplai ";
-var i = Math.floor(5*Math.random());
+
 var rtt9 = new Array ();
 rtt9[0] = "untuk Anda ";
 rtt9[1] = "ke Customer Anda ";
 rtt9[2] = "untuk Pelanggan setia Anda";
 rtt9[3] = "ke pelanggan anda ";
 rtt9[4] = " ";
-var i = Math.floor(5*Math.random());
 
 var r_t = new Array ();
 r_t[0] = "Perkenalkan kami dari ";
@@ -35,7 +34,6 @@ r_t[3] = "Ini Persembahan dari ";
 r_t[4] = "Informasi untuk Anda, dari ";
 r_t[5] = "Persembahan dari ";
 r_t[6] = "Tambahan info bagi Anda, dari ";
-var i = Math.floor(7*Math.random())
 
 var rt = new Array ();
 rt[0] = ". Kali ini kami bagikan untuk anda, informasi ";
@@ -45,7 +43,6 @@ rt[3] = ". Info tambahan ";
 rt[4] = ". Dapatkan info penjualan, spesifikasi, dan hal yang lain ";
 rt[5] = ". Disini Anda bisa mencari info tambahan lebih lengkap ";
 rt[6] = ". Kami senang bisa membagikan info produk, penjualan dan lain-lain ";
-var i = Math.floor(7*Math.random())
 
 var rrt = new Array ();
 rrt[0] = "admin ";
@@ -53,7 +50,6 @@ rrt[1] = "marketing ";
 rrt[2] = "petugas ";
 rrt[3] = "kontak ";
 rrt[4] = "nomor WA ";
-var i = Math.floor(5*Math.random())
 
 var rtt = new Array ();
 rtt[0] = " artikel ";
@@ -61,24 +57,23 @@ rtt[1] = " tulisan ";
 rtt[2] = "  post ";
 rtt[3] = " postingan ";
 rtt[4] = " materi ";
-var i = Math.floor(5*Math.random())
 
 var rt1 = new Array ();
 rt1[0] = " informasi ";
 rt1[1] = " manfaat ";
 rt1[2] = "  apa yang anda cari ";
 rt1[3] = " info ";
-var i = Math.floor(4*Math.random())
+
 var rt2 = new Array ();
 rt2[0] = " semoga ";
 rt2[1] = " mudah-mudahan ";
 rt2[2] = " kami harap ";
 rt2[3] = " untuk semaksimal mungkin ";
-var i = Math.floor(4*Math.random())
 var rt3 = new Array ();
 rt3[0] = " mengenai ";
 rt3[1] = " tentang ";
 rt3[2] = " perihal ";
 rt3[3] = " dalam hal ";
-var i = Math.floor(4*Math.random())
 
+var str = document.title;
+var jud = str.substring(59);
