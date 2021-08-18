4a37499e89a02ce5c01cf395b8bae99793b3e841
suwur <85727752+suwur@users.noreply.github.com>
2021-08-18 15:23:25 +0700

    Create rpagar.js

---
+++ rpagar.js
@@ -0,0 +1,80 @@
+var rrt6 = new Array ();
+rrt6[0] = " mendapat ";
+rrt6[1] = " mendapatkan ";
+rrt6[2] = " memperoleh ";
+rrt6[3] = " dapat ";
+rrt6[4] = " diberikan ";
+
+var rtt7 = new Array ();
+rtt7[0] = " info ";
+rtt7[1] = " informasi ";
+rtt7[2] = " balasan ";
+rtt7[3] = " kabar ";
+rtt7[4] = " data ";
+
+var rrt8 = new Array ();
+rrt8[0] = "Melayani ";
+rrt8[1] = "Siap datang ";
+rrt8[2] = "Kami hadir ";
+rrt8[3] = "Siap melayani ";
+rrt8[4] = "Siap layani ";
+
+var rtt9 = new Array ();
+rtt9[0] = "khusus untuk Anda ";
+rtt9[1] = "untuk kebutuhan Anda ";
+rtt9[2] = "untuk Pelanggan setia Anda";
+rtt9[3] = "ke tempat anda ";
+rtt9[4] = "spesial untuk user Anda ";
+
+var r_t = new Array ();
+r_t[0] = "Perkenalkan kami dari ";
+r_t[1] = "Selamat datang di ";
+r_t[2] = "Halo Sahabat ";
+r_t[3] = "Ini Persembahan dari ";
+r_t[4] = "Informasi untuk Anda, dari ";
+r_t[5] = "Persembahan dari ";
+r_t[6] = "Tambahan info bagi Anda, dari ";
+
+var rt = new Array ();
+rt[0] = ". Kali ini kami bagikan untuk anda, informasi ";
+rt[1] = ". Inilah informasi tambahan untuk Anda, ";
+rt[2] = ". Kami senang berbagi dengan Anda, kali ini ";
+rt[3] = ". Info tambahan ";
+rt[4] = ". Dapatkan info penjualan, spesifikasi, dan hal yang lain ";
+rt[5] = ". Disini Anda bisa mencari info tambahan lebih lengkap ";
+rt[6] = ". Kami senang bisa membagikan info produk, penjualan dan lain-lain ";
+
+var rrt = new Array ();
+rrt[0] = "admin ";
+rrt[1] = "marketing ";
+rrt[2] = "petugas ";
+rrt[3] = "kontak ";
+rrt[4] = "nomor WA ";
+
+var rtt = new Array ();
+rtt[0] = " artikel ";
+rtt[1] = " tulisan ";
+rtt[2] = "  post ";
+rtt[3] = " postingan ";
+rtt[4] = " materi ";
+
+var rt1 = new Array ();
+rt1[0] = " informasi ";
+rt1[1] = " manfaat ";
+rt1[2] = "  apa yang anda cari ";
+rt1[3] = " info ";
+
+var rt2 = new Array ();
+rt2[0] = " semoga ";
+rt2[1] = " mudah-mudahan ";
+rt2[2] = " kami harap ";
+rt2[3] = " untuk semaksimal mungkin ";
+var rt3 = new Array ();
+rt3[0] = " mengenai ";
+rt3[1] = " tentang ";
+rt3[2] = " perihal ";
+rt3[3] = " dalam hal ";
+rt3[4] = " untuk ";
+
+var str = document.title;
+var jud = str.substring(59);
