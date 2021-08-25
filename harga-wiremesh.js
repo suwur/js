ce67c40ab7dbb8af131bbefc11d5b59bc507bc0e
suwur <85727752+suwur@users.noreply.github.com>
2021-08-25 13:13:38 +0700

    Update harga-wiremesh.js

--- harga-wiremesh.js
+++ harga-wiremesh.js
@@ -1,5 +1,5 @@
 
-document.getElementById("afan-menu").insertAdjacentHTML("afterend",
+document.getElementById("hw").insertAdjacentHTML("afterend",
 "<br><br><div id='hitung'>Diameter (mm) : <input type='text' onchange='hitung()' onmouseout='hitung()' id='dia' value='6'><br><br>Jenis <span id='tra'>_________</span>: <select id='jenisW' onChange='hitung()'><option value='0' selected>Standar</option><option value='1'>OverHang</option></select><br><br>	<span id='result'></span><br><input type='button' onclick='hitung()' value=' Hitung '></div><br><br><br><br><br><style>select, #hitung, #result, #hasil, #hitung input[type='text'], #hitung input[type='button'] {font-size: 130%; line-height: 1.2; font-weight:bold} #hitung input, select, button{background-color:#333}input[type='text']{max-width: 190px;}#res {font-size: 60%; }#tra {color: transparent;}</style>");
 
 function hitung(){
