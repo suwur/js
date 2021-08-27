e65146413afb6cf2acf7a8c8cc64dff5c255ebad
suwur <85727752+suwur@users.noreply.github.com>
2021-08-27 11:05:25 +0700

    Update rarsitektur.js

--- rarsitektur.js
+++ rarsitektur.js
@@ -1,3 +1,4 @@
+document.getElementById("mmhb").insertAdjacentHTML("afterend","123 123 123 ");
 document.getElementById("mmhb").insertAdjacentHTML("afterend",
-"<style scoped='' type='text/css'>#rp li{list-style-type:none;margin:33px 0;float:left;height:100px}#rp a{font-size:1.1rem}#rp img{border-radius:10px;float:left;margin-right:5px}.dudu li{width:49%;background:#fff}@media(min-width:1045px){.dudu li{width:33%}}@media(max-width:600px){.dudu li{width:99%}}</style><ul id='rp' class='dudu'>");
+"membuat tulisan disini<style scoped='' type='text/css'>#rp li{list-style-type:none;margin:33px 0;float:left;height:100px}#rp a{font-size:1.1rem}#rp img{border-radius:10px;float:left;margin-right:5px}.dudu li{width:49%;background:#fff}@media(min-width:1045px){.dudu li{width:33%}}@media(max-width:600px){.dudu li{width:99%}}</style><ul id='rp' class='dudu'>");
 document.getElementById("mmhb").insertAdjacentHTML("afterend","</ul>");
