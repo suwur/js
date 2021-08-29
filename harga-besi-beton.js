50567db1fa7f8dcf089339b2f1e2fdd57e6b3dff
suwur <85727752+suwur@users.noreply.github.com>
2021-08-29 18:28:27 +0700

    Update harga-besi-beton.js

--- harga-besi-beton.js
+++ harga-besi-beton.js
@@ -1,6 +1,12 @@
 document.getElementById("hb").insertAdjacentHTML("afterend",
 "<br><br><div id='bhitung'>Panjang (m) <span id='tra'>___</span>: <input type='text' onmouseout='bhitung()' onchange='bhitung()' id='bpjg' value='12'><br><br>Diameter (mm) : <input type='text' onchange='bhitung()' onmouseout='bhitung()' id='bdia' autofocus='' value='10'><br><br><div><span id='bhasil'><span id='bresult'></span></span></div><br><input type='button' onclick='bhitung()' value=' Hitung '></div><br><style>#bhitung,#bresult,#hasil,#bhitung input[type='text'],#bhitung input[type='button']{font-size:130%;line-height:1.2;font-weight:bold}input[type='text']{max-width:140px}#res{font-size:60%}#tra{color:transparent}.kec{font-size:90%;font-weight:100}</style><span id='subb'></span>");
 
+function fR(angka){
+var reverse = angka.toString().split('').reverse().join(''),
+ribuan = reverse.match(/\d{1,3}/g);
+ribuan = ribuan.join('.').split('').reverse().join('');
+}
+
 var be = 0;
 function bhitung()
 { 
@@ -12,7 +18,7 @@ bberat = bbrt.toFixed(2);
   else if (bdi > 12.9){bhbes = Number(hbesi) + 50}
   else{bhbes = Number(hbesi)}
 bharga = bhbes * bberat;
-document.getElementById("bresult").innerHTML = "<span id = 'res'> Berat Besi Beton (per batang) :  </span>" + bberat + "<span id = 'res'> kg <br>HARGA Besi Beton : Rp "+bhbes.toFixed(3)+" per kg<br> = Rp </span>"+bharga.toFixed(0)+"<span id = 'res'> per batang</span>";
+document.getElementById("bresult").innerHTML = "<span id = 'res'> Berat Besi Beton (per batang) :  </span>" + bberat + "<span id = 'res'> kg <br>HARGA Besi Beton : Rp "+bhbes.toFixed(3)+" per kg<br> = Rp </span>"+bharga.toFixed(0)+" = +fR(bharga)+<span id = 'res'> per batang</span>";
 if (be < 1){
 document.getElementById("bhitung").insertAdjacentHTML("afterend",
 "<br><br><a class='kec'><input type='button' onclick='bhitu()' value=' Semua Ukuran '></a><br>");
@@ -27,21 +33,10 @@ bberat = bbrt.toFixed(2);
   else{bhbes = Number(hbesi)}
 }
 
-function fR(angka, prefix)
-	{
-		var number_string = angka.replace(/[^,\d]/g, '').toString(),
-			split	= number_string.split(','),
-			sisa 	= split[0].length % 3,
-			rupiah 	= split[0].substr(0, sisa),
-			ribuan 	= split[0].substr(sisa).match(/\d{3}/gi);
-			
-		if (ribuan) {
-			separator = sisa ? '.' : '';
-			rupiah += separator + ribuan.join('.');
-		}
-		
-		rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
-		return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
+function fR(angka){
+var	reverse = angka.toString().split('').reverse().join(''),
+	ribuan 	= reverse.match(/\d{1,3}/g);
+	ribuan	= ribuan.join('.').split('').reverse().join('');
 	}
 
 function bhitu(){
