d9858da90f28ca434ec5664eacfd4625152f87a9
suwur <85727752+suwur@users.noreply.github.com>
2021-08-29 18:57:19 +0700

    Update harga-besi-beton.js

--- harga-besi-beton.js
+++ harga-besi-beton.js
@@ -34,17 +34,17 @@ bberat = bbrt.toFixed(2);
 }
 
 function bhitu(){
-bdi = 6; bphi(); b6 = fR(bhbes * bberat);
-bdi = 8; bphi(); b8 = fR(bhbes * bberat);
-bdi = 10; bphi(); b10 = fR(bhbes * bberat);
-bdi = 12; bphi(); b12 = fR(bhbes * bberat);
-bdi = 13; bphi(); b13 = fR(bhbes * bberat);
-bdi = 16; bphi(); b16 = bfR(bhbes * bberat);
-bdi = 19; bphi(); b19 = fR(bhbes * bberat);
-bdi = 22; bphi(); b22 = fR(bhbes * bberat);
-bdi = 25; bphi(); b25 = fR(bhbes * bberat);
-bdi = 29; bphi(); b29 = fR(bhbes * bberat);
-bdi = 32; bphi(); b32 = fR(bhbes * bberat);
+bdi = 6; bphi(); bb = bhbes * bberat; b6 = fR(bb);
+bdi = 8; bphi(); bb = bhbes * bberat; b8 = fR(bb);
+bdi = 10; bphi(); bb = bhbes * bberat; b10 = fR(bb);
+bdi = 12; bphi(); bb = bhbes * bberat; b12 = fR(bb);
+bdi = 13; bphi(); bb = bhbes * bberat; b13 = fR(bb);
+bdi = 16; bphi(); bb = bhbes * bberat; b16 = fR(bb);
+bdi = 19; bphi(); bb = bhbes * bberat; b19 = fR(bb);
+bdi = 22; bphi(); bb = bhbes * bberat; b22 = fR(bb);
+bdi = 25; bphi(); bb = bhbes * bberat; b25 = fR(bb);
+bdi = 29; bphi(); bb = bhbes * bberat; b29 = fR(bb);
+bdi = 32; bphi(); bb = bhbes * bberat; b32 = fR(bb);
   
 document.getElementById("subb").innerHTML = "⌀6 = Rp "+b6+" per batang <br>⌀8 = Rp "+b8+" per batang <br>⌀10 = Rp "+b10+" per batang <br>⌀12 = Rp "+b12+" per batang <br>⌀16 = Rp "+b16+" per batang <br>⌀19 = Rp "+b19+" per batang <br>⌀25 = Rp "+b25+" per batang <br>⌀32 = Rp "+b32+" per batang <br><br>D10 = Rp "+b10+" / batang <br>D13 = Rp "+b13+" / batang <br>D16 = Rp "+b16+" / batang <br>D19 = Rp "+b19+" / batang <br>D22 = Rp "+b22+" / batang <br>D25 = Rp "+b25+" / batang <br>D29 = Rp "+b29+" / batang <br>D32 = Rp "+b32+" / batang <br>";
 if (be < 2){document.body.scrollTop += 290;document.documentElement.scrollTop += 290;be = be + 1;
