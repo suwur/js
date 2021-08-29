d87339ef4ee60d662d0018e500a9ef1dfff741e0
suwur <85727752+suwur@users.noreply.github.com>
2021-08-29 19:12:33 +0700

    Update harga-besi-beton.js

--- harga-besi-beton.js
+++ harga-besi-beton.js
@@ -2,7 +2,7 @@ document.getElementById("hb").insertAdjacentHTML("afterend",
 "<br><br><div id='bhitung'>Panjang (m) <span id='tra'>___</span>: <input type='text' onmouseout='bhitung()' onchange='bhitung()' id='bpjg' value='12'><br><br>Diameter (mm) : <input type='text' onchange='bhitung()' onmouseout='bhitung()' id='bdia' autofocus='' value='10'><br><br><div><span id='bhasil'><span id='bresult'></span></span></div><br><input type='button' onclick='bhitung()' value=' Hitung '></div><br><style>#bhitung,#bresult,#hasil,#bhitung input[type='text'],#bhitung input[type='button']{font-size:130%;line-height:1.2;font-weight:bold}input[type='text']{max-width:140px}#res{font-size:60%}#tra{color:transparent}.kec{font-size:90%;font-weight:100}</style><span id='subb'></span>");
 
 function fR(angka){
-var reverse = angka.toString().split('').reverse().join(''),
+var reverse = angka.toFixed(0).split('').reverse().join(''),
 ribuan = reverse.match(/\d{1,3}/g);
 return ribuan = ribuan.join('.').split('').reverse().join('');
 }
