1b4ca3dca7e37d4e5edd4d42ae6e0441f439f853
suwur <85727752+suwur@users.noreply.github.com>
2021-08-29 18:38:05 +0700

    Update harga-besi-beton.js

--- harga-besi-beton.js
+++ harga-besi-beton.js
@@ -4,7 +4,7 @@ document.getElementById("hb").insertAdjacentHTML("afterend",
 function fR(angka){
 var reverse = angka.toString().split('').reverse().join(''),
 ribuan = reverse.match(/\d{1,3}/g);
-ribuan = ribuan.join('.').split('').reverse().join('');
+return ribuan = ribuan.join('.').split('').reverse().join('');
 }
 
 var be = 0;
