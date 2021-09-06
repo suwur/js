236b6dbda702d7c762fb1d3a17f96c0bb12454f0
suwur <85727752+suwur@users.noreply.github.com>
2021-09-07 05:23:26 +0700

    Create harga-random.js

---
+++ harga-random.js
@@ -0,0 +1,117 @@
+function fR(angka){
+var reverse = angka.toFixed(0).split('').reverse().join(''),
+ribuan = reverse.match(/\d{1,3}/g);
+return ribuan = ribuan.join('.').split('').reverse().join('');
+}
+function s0(ss){
+s100 = ss / 100 + 0.5; return s100 = s100.toFixed(0) * 100; 
+}
+function phi(){
+brt = di * di * 156.6 * 0.006165;
+berat = brt.toFixed(2);
+  if (di < 5.6){hbes = Number(hwmes) + 200}
+  else if (di < 4.6){hbes = Number(hwmes) + 1700}
+  else if (di > 9.6){hbes = Number(hwmes) + 100}
+  else if (di > 10.5){hbes = Number(hwmes) + 200}
+  else if (di > 11.5){hbes = Number(hwmes) + 1450}
+  else{hbes = Number(hwmes)}
+}
+di = 4; phi(); mm = hbes * berat; m4 = fR(s0(mm));mm4 = mm * 10; mm4 = fR(mm4);
+di = 5; phi(); mm = hbes * berat; m5 = fR(s0(mm));mm5 = mm * 10; mm5 = fR(mm5);
+di = 6; phi(); mm = hbes * berat; m6 = fR(s0(mm));mm6 = mm * 10; mm6 = fR(mm6);
+di = 7; phi(); mm = hbes * berat; m7 = fR(s0(mm));
+di = 8; phi(); mm = hbes * berat; m8 = fR(s0(mm));
+di = 9; phi(); mm = hbes * berat; m9 = fR(s0(mm));
+di = 10; phi(); mm = hbes * berat; m10 = fR(s0(mm));
+di = 11; phi(); mm = hbes * berat; m11 = fR(s0(mm));
+di = 12; phi(); mm = hbes * berat; m12 = fR(s0(mm));
+function bphi(){
+bbrt = bdi * bdi * 12 * 0.006165;
+bberat = bbrt.toFixed(2);
+  if (bdi < 6.1){bhbes = Number(hbesi) + 400}
+  else if (bdi > 12.9){bhbes = Number(hbesi) + 50}
+  else{bhbes = Number(hbesi)}
+}
+bdi = 6; bphi(); bb = bhbes * bberat; b6 = fR(s0(bb));
+bdi = 8; bphi(); bb = bhbes * bberat; b8 = fR(s0(bb));
+bdi = 10; bphi(); bb = bhbes * bberat; b10 = fR(s0(bb));
+bdi = 12; bphi(); bb = bhbes * bberat; b12 = fR(s0(bb));
+bdi = 13; bphi(); bb = bhbes * bberat; b13 = fR(s0(bb));
+bdi = 16; bphi(); bb = bhbes * bberat; b16 = fR(s0(bb));
+bdi = 19; bphi(); bb = bhbes * bberat; b19 = fR(s0(bb));
+bdi = 22; bphi(); bb = bhbes * bberat; b22 = fR(s0(bb));
+bdi = 25; bphi(); bb = bhbes * bberat; b25 = fR(s0(bb));
+bdi = 32; bphi(); bb = bhbes * bberat; b32 = fR(s0(bb));
+var bb = new Array ();
+bb[0] = " Termurah";
+bb[1] = " Terbaik";
+bb[2] = " Terlaris";
+bb[3] = " Murah";
+bb[4] = " Laris";
+bb[5] = "";
+bb[6] = " Bulan Ini";
+bb[7] = " Hari Ini";
+bb[8] = " Terkini";
+bb[9] = " Terbaru";
+function count(main_str, sub_str){
+main_str += '';
+sub_str += '';
+if (sub_str.length <= 0){return main_str.length + 1;}
+subStr = sub_str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+return (main_str.match(new RegExp(subStr, 'gi')) || []).length;
+}
+var nui = Math.floor(5*Math.random());var nub = Math.floor(2*Math.random());
+if (nui > 2){var i1 = 0}else{var i1 = 1}
+if (nub > 0){var i1 = 7}
+var jjud = document.title; 
+var byk = count(jjud, 'wiremesh');
+if (byk > 0){var lab = 'Wiremesh'
+s1[0] = "<br> Wiremesh M4 harga = ";
+s1[1] = "<br> Wiremesh M5 harga = ";
+s1[2] = "<br> Wiremesh M6 harga = ";
+s1[3] = "<br> Wiremesh M7 harga = ";
+s1[4] = "<br> Wiremesh M8 harga = ";
+s1[5] = "<br> Wiremesh M9 harga = ";
+s1[6] = "<br> Wiremesh M10 harga = ";
+var s2 = new Array ();
+s2[0] = "Rp "+m4;
+s2[1] = "Rp "+m5;
+s2[2] = "Rp "+m6;
+s2[3] = "Rp "+m7;
+s2[4] = "Rp "+m8;
+s2[5] = "Rp "+m9;
+s2[6] = "Rp "+m10;   
+}
+else{var lab = 'Besi%20Beton'
+var s1 = new Array ();
+s1[0] = "<br> Besi Beton &#8960; 6 harga = ";
+s1[1] = "<br> Besi Beton &#8960; 8 harga = ";
+s1[2] = "<br> Besi Beton &#8960; 10 harga = ";
+s1[3] = "<br> Besi Beton &#8960; 12 harga = ";
+s1[4] = "<br> Besi Beton &#8960; 16 harga = ";
+s1[5] = "<br> Besi Beton &#8960; 19 harga = ";
+s1[6] = "<br> Besi Beton &#8960; 25 harga = ";
+s1[7] = "<br> Besi Beton D 10 harga = ";
+s1[8] = "<br> Besi Beton D 13 harga = ";
+s1[9] = "<br> Besi Beton D 16 harga = ";
+s1[10] = "<br> Besi Beton D 19 harga = ";
+s1[11] = "<br> Besi Beton D 22 harga = ";
+s1[12] = "<br> Besi Beton D 25 harga = ";
+s1[13] = "<br> Besi Beton D 32 harga = ";
+var s2 = new Array ();
+s2[0] = "Rp "+b6;
+s2[1] = "Rp "+b8;
+s2[2] = "Rp "+b10;
+s2[3] = "Rp "+b12;
+s2[4] = "Rp "+b16;
+s2[5] = "Rp "+b19;
+s2[6] = "Rp "+b25;  
+s2[7] = "Rp "+b10;
+s2[8] = "Rp "+b13;
+s2[9] = "Rp "+b16;
+s2[10] = "Rp "+b19;
+s2[11] = "Rp "+b22;
+s2[12] = "Rp "+b25;
+s2[13] = "Rp "+b32;  
+}
+num = 3 + nui;
