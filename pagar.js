6903c1bffbbcd8d1b9c7c286560dd826eb2fba48
suwur <85727752+suwur@users.noreply.github.com>
2021-09-29 18:17:32 +0700

    Create pagar.js

---
+++ pagar.js
@@ -0,0 +1,105 @@
+var rrt6 = new Array ();
+rrt6[0] = " mendapat ";
+rrt6[1] = " mendapatkan ";
+rrt6[2] = " memperoleh ";
+rrt6[3] = " dapat ";
+rrt6[4] = " diberikan ";
+var i = Math.floor(5*Math.random());
+var rrt6i = rrt6[i];
+    
+var rtt7 = new Array ();
+rtt7[0] = " info ";
+rtt7[1] = " informasi ";
+rtt7[2] = " balasan ";
+rtt7[3] = " kabar ";
+rtt7[4] = " data ";
+var i = Math.floor(5*Math.random());
+var rtt7i = rtt7[i];
+
+var rrt8 = new Array ();
+rrt8[0] = "Tersedia ";
+rrt8[1] = "Siap Melayani ";
+rrt8[2] = "Menyediakan ";
+rrt8[3] = "Siap Layani ";
+rrt8[4] = "Siap suplai ";
+var i = Math.floor(5*Math.random());
+var rrt8i = rrt8[i];
+
+var rtt9 = new Array ();
+rtt9[0] = "khusus untuk Anda ";
+rtt9[1] = "ke Customer Anda ";
+rtt9[2] = "untuk Pelanggan setia Anda";
+rtt9[3] = "ke pelanggan anda ";
+rtt9[4] = "spesial untuk user Anda ";
+var i = Math.floor(5*Math.random());
+var rtt9i = rtt9[i];
+
+var r_t = new Array ();
+r_t[0] = "Perkenalkan kami dari ";
+r_t[1] = "Selamat datang di ";
+r_t[2] = "Halo Sahabat ";
+r_t[3] = "Ini Persembahan dari ";
+r_t[4] = "Informasi untuk Anda, dari ";
+r_t[5] = "Persembahan dari ";
+r_t[6] = "Tambahan info bagi Anda, dari ";
+var i = Math.floor(7*Math.random());
+var r_ti = r_t[i];
+
+var rt = new Array ();
+rt[0] = ". Kali ini kami bagikan untuk anda, informasi ";
+rt[1] = ". Inilah informasi tambahan untuk Anda, ";
+rt[2] = ". Kami senang berbagi dengan Anda, kali ini ";
+rt[3] = ". Info tambahan ";
+rt[4] = ". Dapatkan info layanan, spesifikasi, dan hal yang lain ";
+rt[5] = ". Disini Anda bisa mencari info tambahan lebih lengkap ";
+rt[6] = ". Kami senang bisa membagikan info produk, penjualan dan lain-lain ";
+var i = Math.floor(7*Math.random());
+var rti = rt[i];
+
+var rrt = new Array ();
+rrt[0] = "admin ";
+rrt[1] = "marketing ";
+rrt[2] = "petugas ";
+rrt[3] = "kontak ";
+rrt[4] = "nomor WA ";
+var i = Math.floor(5*Math.random());
+var rrti = rrt[i];
+
+var rtt = new Array ();
+rtt[0] = " artikel ";
+rtt[1] = " tulisan ";
+rtt[2] = " post ";
+rtt[3] = " postingan ";
+rtt[4] = " materi ";
+var i = Math.floor(5*Math.random());
+var rtti = rtt[i];
+
+var rt1 = new Array ();
+rt1[0] = " informasi ";
+rt1[1] = " manfaat ";
+rt1[2] = " apa yang anda cari ";
+rt1[3] = " info ";
+rt1[4] = " sesuatu yang berguna ";
+var i = Math.floor(5*Math.random());
+var rt1i = rt1[i];
+
+var rt2 = new Array ();
+rt2[0] = " semoga ";
+rt2[1] = " mudah-mudahan ";
+rt2[2] = " kami harap ";
+rt2[3] = " untuk semaksimal mungkin ";
+rt2[4] = " dengan harapan ";
+var i = Math.floor(5*Math.random());
+var rt2i = rt2[i];
+
+var rt3 = new Array ();
+rt3[0] = " mengenai ";
+rt3[1] = " tentang ";
+rt3[2] = " perihal ";
+rt3[3] = " dalam hal ";
+rt3[4] = " untuk ";
+var i = Math.floor(5*Math.random());
+var rt3i = rt3[i];
+
+var str = document.title;
+var jud = str.substring(59);
